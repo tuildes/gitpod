73bb82bbebe722b089730a5cce171589f7fadea1
Gustavo Frehse <119139000+tuildes@users.noreply.github.com>
2023-12-13 16:01:01 -0300

    Delete old directory

--- old/resetconfig.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-php artisan migrate:fresh --seed;
-php artisan key:generate;
-php artisan passport:install --force;
-php artisan passport:client --personal;
-php artisan storage:link;
