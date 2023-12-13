492f1a2602f7458e0ceec7049241fdc5154f9692
Gustavo Frehse <119139000+tuildes@users.noreply.github.com>
2023-12-13 16:01:50 -0300

    Create extensions.sh

---
+++ config-gitpod/extensions.sh
@@ -0,0 +1,9 @@
+code --install-extension BeardedBear.beardedtheme;
+code --install-extension adpyke.codesnap;
+code --install-extension Equinusocio.vsc-material-theme;
+code --install-extension equinusocio.vsc-material-theme-icons;
+code --install-extension MS-CEINTL.vscode-language-pack-pt-BR;
+code --install-extension esbenp.prettier-vscode;
+code --install-extension octref.vetur;
+code --install-extension vscode-icons-team.vscode-icons;
+code --install-extension Features Vue.volar;
